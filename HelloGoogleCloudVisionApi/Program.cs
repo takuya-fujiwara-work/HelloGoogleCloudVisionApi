1bc0d2bae9e037c79751125f27e61046e0dc5073
Takuya Fujiwara <mobilefujitaku@gmail.com>
2017-08-15 11:46:48 +0900

    add TEXT_DETECTION

--- HelloGoogleCloudVisionApi/Program.cs
+++ HelloGoogleCloudVisionApi/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Vision.v1;
+using Google.Apis.Vision.v1.Data;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloGoogleCloudVisionApi
 {
@@ -10,6 +11,48 @@ namespace HelloGoogleCloudVisionApi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args.Length);
+            if (args.Length != 1) { Console.WriteLine("Usage:HelloGoogleCloudVisionApi <path_to_image>"); return; }
+
+            string imagePath = args[0];
+            byte[] imageArray = System.IO.File.ReadAllBytes(imagePath);
+            string imageContent = Convert.ToBase64String(imageArray);
+
+            var v = CreateAuthorizedClient();
+            var result = v.Images.Annotate(new BatchAnnotateImagesRequest()
+            {
+                Requests = new[]
+                {
+                    new AnnotateImageRequest() {
+                        Features = new [] { new Feature() { Type =
+                          "TEXT_DETECTION"}},
+                        Image = new Image() { Content = imageContent }
+                    }
+                }
+            }).Execute();
+            result.Responses.ToList().ForEach(a => a.TextAnnotations.ToList().ForEach(b => Console.WriteLine(b.Description)));
+
+            Console.WriteLine("Press any key:");
+            Console.ReadKey();
+
+        }
+
+        public static VisionService CreateAuthorizedClient()
+        {
+            var credential = GoogleCredential.FromJson(System.Configuration.ConfigurationManager.AppSettings["SERVICE_ACCOUNT_JSON"]);
+
+            if (credential.IsCreateScopedRequired)
+            {
+                credential = credential.CreateScoped(new[]
+                {
+                    VisionService.Scope.CloudPlatform
+                });
+            }
+            return new VisionService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                GZipEnabled = false
+            });
         }
     }
 }
